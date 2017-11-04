ba38a2d4e767d2f3ba0d94f01c7cd508fc9c83cf
menhyrDev <vincent.lanore@ens-lyon.org>
2017-11-04 14:59:46 +0100

    Added new people sprites + now using views to scroll and zoom

--- test.cpp
+++ test.cpp
@@ -61,11 +61,10 @@ class TileMap : public sf::Drawable, public sf::Transformable, public Component 
         int shift_hexa = (y % 2 == 0) ? (hd / 2) : 0;
 
         sf::Vertex *quad = &array[index * 4];
-        quad[0].position = sf::Vector2f(offset_x + shift_hexa + x * hd, offset_y + y * vd);
-        quad[1].position = sf::Vector2f(offset_x + shift_hexa + x * hd + size_x, offset_y + y * vd);
-        quad[2].position =
-            sf::Vector2f(offset_x + shift_hexa + x * hd + size_x, offset_y + y * vd + size_y);
-        quad[3].position = sf::Vector2f(offset_x + shift_hexa + x * hd, offset_y + y * vd + size_y);
+        quad[0].position = sf::Vector2f(shift_hexa + x * hd, y * vd);
+        quad[1].position = sf::Vector2f(shift_hexa + x * hd + size_x, y * vd);
+        quad[2].position = sf::Vector2f(shift_hexa + x * hd + size_x, y * vd + size_y);
+        quad[3].position = sf::Vector2f(shift_hexa + x * hd, y * vd + size_y);
         quad[0].texCoords = sf::Vector2f(0, type * size_y);
         quad[1].texCoords = sf::Vector2f(size_x, type * size_y);
         quad[2].texCoords = sf::Vector2f(size_x, (type + 1) * size_y);
@@ -74,14 +73,13 @@ class TileMap : public sf::Drawable, public sf::Transformable, public Component 
 
   public:
     int tile_size{150};
-    int offset_x{-200}, offset_y{-200};
 
     TileMap() { port("map", &TileMap::map); }
 
     void load() {
         size_t size_x = map->size_x, size_y = map->size_y;
 
-        tileset.loadFromFile("alltiles.png");
+        tileset.loadFromFile("png/alltiles.png");
         array.setPrimitiveType(sf::Quads);
         array.resize(4 * size_x * size_y);
 
@@ -113,6 +111,9 @@ class MainLoop : public Component {
         // sf::RenderWindow window(sf::VideoMode(2560, 1080), "Test", sf::Style::Fullscreen);
         window.setFramerateLimit(60);
 
+        sf::View main_view(sf::FloatRect(200, 200, 1700, 1200));
+        window.setView(main_view);
+
         tilemap->load();
 
         // int loops{0};
@@ -132,11 +133,15 @@ class MainLoop : public Component {
                 } else if (event.type == sf::Event::MouseButtonReleased and
                            event.mouseButton.button == sf::Mouse::Left) {
                     mouse_pressed = false;
+                } else if (event.type == sf::Event::MouseWheelScrolled) {
+                    main_view.zoom(1 - (event.mouseWheelScroll.delta * 0.15));
+                    window.setView(main_view);
                 }
             }
             if (mouse_pressed) {
-                tilemap->offset_x += sf::Mouse::getPosition(window).x - mouse_x;
-                tilemap->offset_y += sf::Mouse::getPosition(window).y - mouse_y;
+                main_view.move(mouse_x - sf::Mouse::getPosition(window).x,
+                               mouse_y - sf::Mouse::getPosition(window).y);
+                window.setView(main_view);
                 mouse_x = sf::Mouse::getPosition(window).x;
                 mouse_y = sf::Mouse::getPosition(window).y;
                 tilemap->load();
@@ -151,16 +156,16 @@ class MainLoop : public Component {
             //     start = chrono::system_clock::now();
             // }
 
-            sf::CircleShape hexagon(75, 6);
-            // hexagon.setRotation(90);
-            hexagon.setPosition(400, 100);
-            hexagon.setFillColor(sf::Color(0, 0, 0, 0));
-            hexagon.setOutlineThickness(4);
-            hexagon.setOutlineColor(sf::Color(180, 180, 255, 90));
+            // sf::CircleShape hexagon(75, 6);
+            // // hexagon.setRotation(90);
+            // hexagon.setPosition(400, 100);
+            // hexagon.setFillColor(sf::Color(0, 0, 0, 0));
+            // hexagon.setOutlineThickness(4);
+            // hexagon.setOutlineColor(sf::Color(180, 180, 255, 90));
 
             window.clear();
             window.draw(*tilemap);
-            window.draw(hexagon);
+            // window.draw(hexagon);
             window.display();
         }
     }
