733ebf111ffed6a403f3d0a34bdafc42c15ee8c1
menhyrDev <vincent.lanore@ens-lyon.org>
2017-11-29 22:33:17 +0100

    TerrainMap in separate file + added LGPL header

---
+++ src/TerrainMap.hpp
@@ -0,0 +1,43 @@
+/*Copyright Vincent Lanore 2017
+
+  This file is part of Menhyr.
+
+  Menhyr is free software: you can redistribute it and/or modify it under the terms of the GNU
+  Lesser General Public License as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  Menhyr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with Menhyr. If
+  not, see <http://www.gnu.org/licenses/>.*/
+
+#ifndef TERRAIN_MAP_HPP
+#define TERRAIN_MAP_HPP
+
+#include "HexCoords.hpp"
+
+/*
+====================================================================================================
+  ~*~ TerrainMap ~*~
+==================================================================================================*/
+class TerrainMap : public Component {
+    using TileType = int;
+    std::unordered_map<HexCoords, TileType> map;
+
+  public:
+    TileType get(const HexCoords& coords) {
+        auto it = map.find(coords);
+        if (it == map.end()) {
+            map.insert(std::make_pair(coords, rand() % 7));
+            return map.at(coords);
+        } else {
+            return it->second;
+        }
+    }
+
+    void set(const HexCoords& coords, TileType type) { map[coords] = type; }
+};
+
+#endif  // TERRAIN_MAP_HPP
--- src/globals.hpp
+++ src/globals.hpp
@@ -1,3 +1,21 @@
+/*Copyright Vincent Lanore 2017
+
+  This file is part of Menhyr.
+
+  Menhyr is free software: you can redistribute it and/or modify it under the terms of the GNU
+  Lesser General Public License as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  Menhyr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with Menhyr. If
+  not, see <http://www.gnu.org/licenses/>.*/
+
+#ifndef GLOBALS_HPP
+#define GLOBALS_HPP
+
 #include <math.h>
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
@@ -30,3 +48,5 @@ struct GameObject : public sf::Drawable, public sf::Transformable, public tc::Co
   ~*~ vector operators ~*~
 ==================================================================================================*/
 vec operator/(vec v, scalar s) { return vec(v.x / s, v.y / s); }
+
+#endif  // GLOBALS_HPP
